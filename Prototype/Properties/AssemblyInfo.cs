b2ad8d80acaba5197621e5905f0eed30bb1df553
ermau <me@ermau.com>
2012-04-16 00:50:45 -0400

    Update name

--- Prototype/Properties/AssemblyInfo.cs
+++ Prototype/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LiveCSharp")]
+[assembly: AssemblyTitle("Instant Prototype")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eric Maupin")]
-[assembly: AssemblyProduct("LiveCSharp")]
+[assembly: AssemblyProduct("Instant Prototype")]
 [assembly: AssemblyCopyright("Copyright © Eric Maupin 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
