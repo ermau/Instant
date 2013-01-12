ef4583f7300be009e16ce210e3922ad4233c8982
ermau <me@ermau.com>
2013-01-12 15:20:58 -0500

    Fix // comments in test code

--- Instant/Evaluator.cs
+++ Instant/Evaluator.cs
@@ -161,7 +161,7 @@ namespace Instant
 					cparams.CompilerOptions = next.Project.GetCompilerOptions();
 
 					// HACK: Wrap test code into a proper method
-					string evalSource = "namespace Instant.User { static class Evaluation { static void Evaluate() {" + next.EvalCode + " } } }";
+					string evalSource = "namespace Instant.User { static class Evaluation { static void Evaluate() {" + next.EvalCode + Environment.NewLine + " } } }";
  
 					List<string> sources = next.Project.Sources.AsParallel().Select (
 							e => e.Fold (async f => await f.OpenText().ReadToEndAsync(), Task.FromResult)
